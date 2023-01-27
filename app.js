360ae0e3199fd36cdea4a7b0a8d52e6a8e37b715
lelouch248 <lelouch248@users.noreply.github.com>
2023-01-27 10:46:38 +0530

    first commit RESTFULL API

---
+++ app.js
@@ -0,0 +1,176 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+mongoose.set("strictQuery", true);
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+// Database connection
+mongoose.connect("mongodb://0.0.0.0:27017/wikidb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// model creation
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+//get route
+
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find({}, function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article.");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany({}, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully deleted all articles.");
+      }
+    });
+  });
+
+// app.get("/articles", function (req, res) {
+//   Article.find({}, function (err, foundArticles) {
+//     if (!err) {
+//       res.send(foundArticles);
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+// app.post("/articles", function (req, res) {
+//   const newArticle = new Article({
+//     title: req.body.title,
+//     content: req.body.content,
+//   });
+//   newArticle.save(function (err) {
+//     if (!err) {
+//       res.send("Successfully added a new article.");
+//     } else {
+//       res.send(err);
+//     }
+//   });
+// });
+
+// app.delete("/articles", function (req, res) {
+//   Article.deleteMany({}, function (err) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Successfully deleted all articles.");
+//     }
+//   });
+// });
+
+
+app.route("/articles/:articleTitle")
+.get(function (req, res) {
+  const articleTitle = _.kebabCase(req.params.articleTitle);
+    Article.findOne(
+      { title: req.params.articleTitle },
+      function (err, foundArticle) {
+        if (!err) {
+          res.send(foundArticle);
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+  .put(function (req,res) {
+    Article.findOneAndUpdate(
+      {title: req.params.articleTitle},
+      {title:req.body.title, content:req.body.content},
+      {overwrite:true},
+      function (err) {
+        if (!err) {
+          res.send("Successfully updated article.");
+        }
+        else{
+          console.log(err);
+        }
+      }
+    )
+  })
+  .patch(function (req,res) {
+    Article.findOneAndUpdate(
+      {title: req.params.articleTitle},
+      {$set:{title:req.body.title, content:req.body.content}},
+      function (err) {
+        if (!err) {
+          res.send("Successfully updated article.");
+        }
+        else{
+          console.log(err);
+        }
+      }
+    )
+  })
+  .delete(function (req,res) {
+    Article.deleteOne(
+      {title: req.params.articleTitle},
+      function (err) {
+        if (!err) {
+          res.send("Successfully deleted article.");
+        }
+        else{
+          console.log(err);
+        }
+      }
+    )
+  });
+  
+
+
+// app.get("/articles/:articleTitle", function (req, res) {
+//   Article.findOne(
+//     { title: req.params.articleTitle },
+//     function (err, foundArticle) {
+//       if (!err) {
+//         res.send(foundArticle);
+//       } else {
+//         res.send(err);
+//       }
+//     }
+//   );
+// });
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
